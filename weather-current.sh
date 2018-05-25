4a2d5ff00da46f4d0aaed62d1e9d54f75a6db560
root <root@otih-oith.us.to>
2018-05-25 00:00:12 -0500

    date

--- weather-current.sh
+++ weather-current.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #export currloc=$(geoiplookup -f /usr/share/GeoIP/GeoIPCity.dat  $(last  -1 -i -w --time-format notime | grep -e  'ubuntu\|logged in\|pts/*' | cut -c 19-39)  | cut -d "," -f 5 |  cut -d " " -f 2-)
-export currloc=$(geoiplookup -f /usr/share/GeoIP/GeoIPCity.dat  $(last  -1 -i -w --time-format notime | grep -e  'ubuntu\|logged in\|pts/*' | cut -c 19-39)  |  awk '{print $10}')
+export currloc=$(geoiplookup -f /usr/share/GeoIP/GeoIPCity.dat  $(last  -1 -i -w --time-format notime | grep -e  'ubuntu\|logged in\|pts/*' | cut -c 19-39)  |  awk '{print $9}' | cut -d "," -f 1)
 echo $currloc
 #URL='http://www.accuweather.com/en/de/berlin/10178/weather-forecast/178087'
 URL='https://www.accuweather.com/en/us/'$currloc'/55424/weather-forecast/333902'
