f46620add2ce365fdc0eaaed66ed12b6128a7bd6
root <root@otih-oith.us.to>
2018-05-26 00:00:11 -0500

    date

--- lines.sh
+++ lines.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/parallel --shebang-wrap --pipe /bin/bash
-#!/bin/bash
-
+#!/usr/bin/parallel --shebang-wrap --pipe /bin/bash !/bin/bash
+export bMonth="June"
+export cMonth="December"
+export currentDate=$(date | awk '{print $2}')
 #echo
 for  ((number=1;number <= $(tput cols);number++)) #in {1..$(tput lines)}
 do
 
 echo -ne "#"
 
-done 
+done | lolcat
 echo
 exit 0
+
+#if [ echo -ne $inv  $bMonth = echo -ne  $currentDate  $reset ]  || [ echo -ne  $inv  $cMonth =  echo -ne  $currentDate  $reset ]
---
+++ password.sh
@@ -0,0 +1,10 @@
+unset password;
+while IFS= read -r -s -n1 pass; do
+  if [[ -z $pass ]]; then
+     echo
+     break
+  else
+     echo -n '*'
+     password+=$pass
+  fi
+done
