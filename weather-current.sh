c232f4454b235060bfba015099dffb39eeadb12f
root <root@otih-oith.us.to>
2018-06-14 00:00:13 -0500

    date

--- weather-current.sh
+++ weather-current.sh
@@ -11,6 +11,7 @@ echo $currloc
 #URL='http://www.accuweather.com/en/de/berlin/10178/weather-forecast/178087'
 URL='https://www.accuweather.com/en/us/'$currloc'/55424/weather-forecast/333902'
 #wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", "$12"°" }'| head -1
-wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", "$12"°", $13 }'| head -1 | cut -d '"' -f 1-2 | awk -F'text:"' '{print $1 $2}'
-exit 1
+#wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", "$12"°", $13 }'| head -1 | cut -d '"' -f 1-2 | awk -F'text:"' '{print $1 $2}'
 
+wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", Really "$10", Feels like "$12"°", $13  }' | head -1 | cut -d '"' -f 1-2 | awk -F'text:"' '{print $1 $2}'
+exit 1
--- weather.sh
+++ weather.sh
@@ -3,5 +3,6 @@
 #URL='http://www.accuweather.com/en/de/berlin/10178/weather-forecast/178087'
 URL='https://www.accuweather.com/en/us/edina-mn/55424/weather-forecast/333902'
 #wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", "$12"°" }'| head -1
-wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", "$12"°", $13 }'| head -1 | cut -d '"' -f 1-2 | awk -F'text:"' '{print $1 $2}'
+#wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", "$12"°", $13 }'| head -1 | cut -d '"' -f 1-2 | awk -F'text:"' '{print $1 $2}'
+wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", Really "$10", Feels like "$12"°", $13  }' | head -1 | cut -d '"' -f 1-2 | awk -F'text:"' '{print $1 $2}'
 exit 1
