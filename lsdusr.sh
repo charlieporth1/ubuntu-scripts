d37b6eae708c6aa6cb88fd4fabae189eab843e0b
root <root@otih-oith.us.to>
2018-06-27 00:00:11 -0500

    date

--- lsdusr.sh
+++ lsdusr.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
 #!/bin/bash
 #echo
-for  ((number=1;number < $(tput cols);number++)) #in {1..$(tput lines)}
+#for ((number=1;number<$(tput cols);number++))  #in {1..$(tput lines)}
+for ((n=1;n<$(tput cols);n++))  #in {1..$(tput lines)}
+#for  number in {1...$(tput cols)}
 do
-
-echo -ne "$"
-
-done|lolcat -a -d 500
+#echo -ne $number
+echo -ne  "$"
+done|lolcat -a -d 500 
 echo
 exit
