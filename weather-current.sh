f0d4b87e439e82675fdd726c1e8c9c7bff4c06dd
root <root@otih-oith.us.to>
2018-06-29 00:00:12 -0500

    date

--- weather-current.sh
+++ weather-current.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/parallel --shebang-wrap --pipe /bin/bash
-#!/bin/sh
+#!/usr/bin/parallel --shebang-wrap --pipe /bin/bash !/bin/sh
 
 #export currloc=$(geoiplookup -f /usr/share/GeoIP/GeoIPCity.dat  $(last  -1 -i -w --time-format notime | grep -e  'ubuntu\|logged in\|pts/*' | cut -c 19-39)  | cut -d "," -f 5 |  cut -d " " -f 2-)
 #export currentip=$(last  -1 -i -w --time-format notime | grep -e  'ubuntu\|logged in\|pts/*' | cut -c 19-39)
@@ -8,6 +7,14 @@ export mosh="0.0.0.0"
 #if (echo $black $currentip =  $mosh ) then
 export currloc=$(geoiplookup -f /usr/share/GeoIP/GeoIPCity.dat  $(last -1 --time-format notime | grep -e  'ubuntu\|logged in\|pts/*' | awk '{print $3}')  |  awk '{print $9}' | cut -d "," -f 1)
 echo $currloc
+if [ -z !$(echo $currloc | awk '{print $2}') ]; then
+finshiedloc=$( echo $currloc | awk '{print $1 "%20" $2}')
+elif [ -z !$(echo $currloc | awk '{print $3}') ]; then
+finshiedloc=$( echo $currloc | awk '{print $1 "%20" $2 "%20" $3}')
+else 
+finishedloc=$currloc
+fi
+
 
 #URL='http://www.accuweather.com/en/de/berlin/10178/weather-forecast/178087'
 #URL='https://www.accuweather.com/en/us/'$currloc'/55424/weather-forecast/333902'
@@ -15,5 +22,5 @@ echo $currloc
 #wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", "$12"°", $13 }'| head -1 | cut -d '"' -f 1-2 | awk -F'text:"' '{print $1 $2}'
 
 #wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", Really "$10", Feels like "$12"°", $13  }' | head -1 | cut -d '"' -f 1-2 | awk -F'text:"' '{print $1 $2}'
-ansiweather -l $currloc -u imperial -a true -s true
+ansiweather -l $finshedloc -u imperial -a true -s true
 exit 1
