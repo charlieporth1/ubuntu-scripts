20e69eb1b914c142a0651a083f71ca7962460795
root <root@otih-oith.us.to>
2018-06-30 00:00:13 -0500

    date

--- weather-current.sh
+++ weather-current.sh
@@ -3,16 +3,16 @@
 #export currloc=$(geoiplookup -f /usr/share/GeoIP/GeoIPCity.dat  $(last  -1 -i -w --time-format notime | grep -e  'ubuntu\|logged in\|pts/*' | cut -c 19-39)  | cut -d "," -f 5 |  cut -d " " -f 2-)
 #export currentip=$(last  -1 -i -w --time-format notime | grep -e  'ubuntu\|logged in\|pts/*' | cut -c 19-39)
 export  currentip=$(last  -1 -i -w --time-format notime | grep -e  'ubuntu\|logged in\|pts/*' | awk '{print $3}')
-export mosh="0.0.0.0"
+#export mosh="0.0.0.0"
 #if (echo $black $currentip =  $mosh ) then
 export currloc=$(geoiplookup -f /usr/share/GeoIP/GeoIPCity.dat  $(last -1 --time-format notime | grep -e  'ubuntu\|logged in\|pts/*' | awk '{print $3}')  |  awk '{print $9}' | cut -d "," -f 1)
 echo $currloc
 if [ -z !$(echo $currloc | awk '{print $2}') ]; then
-finshiedloc=$( echo $currloc | awk '{print $1 "%20" $2}')
+export finishiedloc=$( echo $currloc | awk '{print $1 "%20" $2}')
 elif [ -z !$(echo $currloc | awk '{print $3}') ]; then
-finshiedloc=$( echo $currloc | awk '{print $1 "%20" $2 "%20" $3}')
+export finishiedloc=$( echo $currloc | awk '{print $1 "%20" $2 "%20" $3}')
 else 
-finishedloc=$currloc
+export finishedloc=$currloc
 fi
 
 
@@ -22,5 +22,5 @@ fi
 #wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", "$12"°", $13 }'| head -1 | cut -d '"' -f 1-2 | awk -F'text:"' '{print $1 $2}'
 
 #wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", Really "$10", Feels like "$12"°", $13  }' | head -1 | cut -d '"' -f 1-2 | awk -F'text:"' '{print $1 $2}'
-ansiweather -l $finshedloc -u imperial -a true -s true
+ansiweather -l $finishedloc -u imperial -a true -s true
 exit 1
