17a9647dc56a9faea05557d73464d518337f5113
root <root@otih-oith.us.to>
2018-09-22 00:57:42 -0500

    Sat Sep 22 00:57:42 CDT 2018

---
+++ server/sshbannersetup.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+
+echo -e "Charles Timothy Porths Website OTIH-OITH http://otih-oith.us.to hosted by" >> /etc/issue.net
+echo -e "$(uname -s)  $HOSTNAME  $HOSTTYPE  " >> /etc/issue.net
+echo -e "You Will be prosecuted, China or anyone who hacks this server if" >> /etc/issue.net
+echo -e "the goverment doesn't I will, whether illegal or legal metheds I will find you" >> /etc/issue.net
+echo -e "and Destroy your life you will not fuck up mine. You will be sorry." >> /etc/issue.net
+echo -e "I will use Kali linux and hydra if I have to I will fuck you up and I hate using the " >> /etc/issue.net
+echo -e "because they take to damn long " >> /etc/issue.net
+echo -e "I will exploit your computer wipe it and demand a rasnom and you will not get you god damn data back" >> /etc/issue.net
+echo -e "I will kill you if you hack me I swear to god I will fly to where you live and murrder you if I god damn needed." >> /etc/issue.net
+echo -e "Its not hard to track you using geoip I can get the street and even the house you live on you should know that my auth logs keep you ip dumnasses " >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "Вы будете привлечены к ответственности, Китай или любой, кто взломает этот сервер, если" >> /etc/issue.net
+echo -e "правительство не будет, будь то незаконные или законные меры, я найду вас" >> /etc/issue.net
+echo -e "и Относитесь к своей жизни, вы не испортите мою. Вы пожалеете." >> /etc/issue.net
+echo -e "Я буду использовать Kali linux и hydra, если мне нужно будет трахнуть вас, и я ненавижу использовать" >> /etc/issue.net
+echo -e "потому что они берут чертовски долго" >> /etc/issue.net
+echo -e "Я буду использовать ваш компьютер, вытереть его и потребовать раснома, и вы не получите вам чертовски данные назад" >> /etc/issue.net
+echo -e "Я убью тебя, если ты взломаешь меня, я клянусь богом, Я полечу туда, где ты живешь, и мчашься, если я чертовски нужу." >> /etc/issue.net
+echo -e "Vy budete privlecheny k otvetstvennosti, Kitay ili lyuboy, kto vzlomayet etot server, yesli" >> /etc/issue.net
+echo -e "pravitel'stvo ne budet, bud' to nezakonnyye ili zakonnyye mery, ya naydu vas" >> /etc/issue.net
+echo -e "i Otnosites' k svoyey zhizni, vy ne isportite moyu. Vy pozhaleyete." >> /etc/issue.net
+echo -e "YA budu ispol'zovat' Kali linux i hydra, yesli mne nuzhno budet trakhnut' vas, i ya nenavizhu ispol'zovat'" >> /etc/issue.net
+echo -e "potomu chto oni berut chertovski dolgo" >> /etc/issue.net
+echo -e "YA budu ispol'zovat' vash komp'yuter, vyteret' yego i potrebovat' rasnoma, i vy ne poluchite vam chertovski dannyye nazad" >> /etc/issue.net
+echo -e "YA ub'yu tebya, yesli ty vzlomayesh' menya, ya klyanus' bogom, YA polechu tuda, gde ty zhivesh', i mchash'sya, yesli ya chertovski nuzhu." >> /etc/issue.net
+echo -e "Его не сложно отследить, используя геоинформацию. Я могу получить улицу и даже дом, в " >> /etc/issue.net
+echo -e "котором вы живете, вы должны знать, что мои журналы auth хранят вас ip dumnasses" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "如果您遭到起诉，中国或任何侵犯此服务器的人将受到起诉" >> /etc/issue.net
+echo -e "政府不是我会的，无论是非法还是法律方面，我都会找到你" >> /etc/issue.net
+echo -e "和失去你的生活，你不会搞砸我的。你会后悔的。" >> /etc/issue.net
+echo -e "我将使用Kali Linux和九头蛇，如果我不得不将我操你，我讨厌使用它" >> /etc/issue.net
+echo -e "因为他们花了很长时间" >> /etc/issue.net
+echo -e "我会利用你的电脑擦拭它，并要求rasnom，你不会让你该死的数据回来" >> /etc/issue.net
+echo -e "我会杀了你，如果你劈开我，我向上帝发誓我会飞到你住的地方，如果我上帝该死的话需要诅咒你。" >> /etc/issue.net
+echo -e "它不難跟踪你使用geoip我可以得到街道，甚至你住在你的房子應該知道我的身份驗證日誌讓你ip dumnasses" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "당신은 기소 당할 것이며, 중국이나이 서버를 해킹하는 사람이라면" >> /etc/issue.net
+echo -e "정부는 불법 또는 법적인 법적인 여부와 상관없이 내가 당신을 찾을 것입니다." >> /etc/issue.net
+echo -e "그리고 Destroy 너의 인생 너는 내 꺼야하지 않을거야. 죄송합니다." >> /etc/issue.net
+echo -e "나는 너를 엿먹 이기만하면 칼리 리눅스와 히드라를 쓸거야." >> /etc/issue.net
+echo -e "그들이 오래 걸리니까." >> /etc/issue.net
+echo -e "나는 컴퓨터를 닦아서 rasnom을 요구할 것이고 당신은 신의 데이터를 돌려받지 못할 것이다." >> /etc/issue.net
+echo -e "네가 날 해킹하면 널 죽일거야. 맹세코 네가 사는 곳으로 날아갈거야." >> /etc/issue.net
+echo -e "지오 팁을 사용하여 당신을 추적하는 것이 어렵지 않습니다. 거리를 얻을 수 있고 심지어 당신이 살고있는 " >> /etc/issue.net
+echo -e "집까지도 내 승인 로그가 당신에게 ip dumnasses를 유지한다는 것을 알아야합니다." >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "You have been warned." >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "Вы были предупреждены." >> /etc/issue.net
+echo -e "你被警告了。" >> /etc/issue.net
+echo -e "당신은 경고를 받았습니다." >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "          :oooooooooooooooooooooooooooooooo/                 " >> /etc/issue.net
+echo -e "                        :oooooooooooooooooooooooooooooooo/                 " >> /etc/issue.net
+echo -e "                   `.--:-..---:/++ooooooooooooooooooooooo/                 " >> /etc/issue.net
+echo -e "              `.:/+o++//-....`   `.-/+ooooooooooooooooooo/                 " >> /etc/issue.net
+echo -e "           .:/+o+:-``   :oooo++/:.   `.:+oooooooooooooooo/                 " >> /etc/issue.net
+echo -e "        .:+oo+-`   `.--:-...-:/+oo+:.   `-/oooooooooooooo/                 " >> /etc/issue.net
+echo -e "     `-/+oo/.   `-/+++//-.`    `.:+o+/.    ./oooooooooooo/                 " >> /etc/issue.net
+echo -e "    `+oooo-   ./+o+:.`  :++/.     -ooo+.    .+ooooooooooo/                 " >> /etc/issue.net
+echo -e "     -oooo:   -ooo/     :ooo+- `-/ooo/.   `:+oooooooooooo/                 " >> /etc/issue.net
+echo -e "      -+ooo:   -+oo:`   :ooooo/+ooo/.   .:+ooo+:/+ooooooo/                 " >> /etc/issue.net
+echo -e "       ./ooo/`  ./oo/.` :oooooo++:.  `-/+oo+/-`  `.:/+ooo/                 " >> /etc/issue.net
+echo -e "        `:+oo+-` `-/+o+::////:-`` `-/+ooo/:.        ./ooo/                 " >> /etc/issue.net
+echo -e "          `:+oo/-`  `-:/-````..-:/+oo+/:.`      `.:/+oooo/                 " >> /etc/issue.net
+echo -e "            `:+oo+/-.`  :+++++o++//:.`     ``.:/+oooooooo/                 " >> /etc/issue.net
+echo -e "               .-/++++//-.--...``   ```.-:/++oooooooooooo/                 " >> /etc/issue.net
+echo -e "                  ``.-:/-......--://+++oooooooooooooooooo/                 " >> /etc/issue.net
+echo -e "                        :oooooooooooooooooooooooooooooooo/                 " >> /etc/issue.net
+echo -e "                        -////////////////////////////////:                 " >> /etc/issue.net
+echo -e "                                                                           " >> /etc/issue.net
+echo -e "                                                                           " >> /etc/issue.net
+echo -e "``````````    ````      ```` ````  `````````      ````      ``````         " >> /etc/issue.net
+echo -e "mmmmmmmmmdy/` smmm-    -mmm/ ommm` ommmmmmmdho-  `dmms     .hmmmmy`        " >> /etc/issue.net
+echo -e "MMMmsssydMMMy -NMMy   `dMMd` sMMM` sMMNo+osmMMN+ `mMMy    `yMMydMMs        " >> /etc/issue.net
+echo -e "MMMh     yMMN- oMMM:  +MMN-  sMMM` sMMN.   `yMMm.`mMMy    +MMd`-NMM/       " >> /etc/issue.net
+echo -e "MMMh     :MMM: `dMMd``mMMo   sMMM` sMMN.    +MMM-`mMMy   :NMN-  +MMN-      " >> /etc/issue.net
+echo -e "MMMh     :MMM:  :NMM/sMMd`   sMMM` sMMN.   -dMMm``mMMy  .mMMmsssyNMMd.     " >> /etc/issue.net
+echo -e "MMMh     :MMM:   sMMNMMN:    sMMM` sMMMhhhmMMMm: `mMMy `hMMhooooooNMMy`-//:" >> /etc/issue.net
+echo -e "yyyo     -yyy.   `yyyyyo     /yyy` +yyyyyyyso/`  `syy+ /yyy.      /yyy:-++/" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "::::::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,:::::::::" >> /etc/issue.net
+echo -e ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" >> /etc/issue.net
+echo -e "::::::::::::::::::::::::::::::,,,::,,,:::::::::::::::::::::::::::::" >> /etc/issue.net
+echo -e ":::::::::::::::::::::::::::,,,+@@@@@@',:,,:::::::::::::::::::::::::" >> /etc/issue.net
+echo -e "::::::::::::::::::::::,,,,,,#@@@@@@@@@@;,,,,,,:::::::::::::::::::::" >> /etc/issue.net
+echo -e "::::::::::::::::::::,,,,,,,@@@@@@@@@@@@@+,,,,,,,:::::::::::::::::::" >> /etc/issue.net
+echo -e ":::::::::::::::::::,,,,,,,@@@@@@@;,,.'@@@',,,,,,,,:::::::::::::::::" >> /etc/issue.net
+echo -e ":::::::::::::::::,,,,,,,.@@@@@@@,,,,,,;@@@,,,,,,,,,::::::::::::::::" >> /etc/issue.net
+echo -e ":::::::::::::::,,,,,,,,,+@@@@@@+,,,,,,,@@@@,,,,,,,,,,::::::::::::::" >> /etc/issue.net
+echo -e "::::::::::::::,,,,,,,,,,@@@@@@@.,,,,,,,;@@@,,,,,,,,,,,:::::::::::::" >> /etc/issue.net
+echo -e ",::::::::::::,,,,,,,,,,+@@@@@@@,,,,,,,.:@@@#,,,,,,,,,,,::::::::::::" >> /etc/issue.net
+echo -e ",:::::::::::,,,,,,,,,,.@@@@@@@+,,,,,.+@@@@@@.,,,,,,,,,,,,::::::::::" >> /etc/issue.net
+echo -e ",::::::::::,,,,,,,,,,,,@@@@@@@;,,,,.#@@@@@@@.,,,,,,,,,,,,,:::::::::" >> /etc/issue.net
+echo -e ",:::::::::,,,,,,,,,,,,;@@@@@@@,,,,,'@@@@@@@@,,,,,,,,,,,,,,:::::::::" >> /etc/issue.net
+echo -e ",::::::::,,,,,,,,,,,,,+@@@@@@@,,,,.@@@@@@@@@,,,,,,,,,,:,,,,,,::::::" >> /etc/issue.net
+echo -e ",:::::::,,,,,,,,,,,,,,#@@@@@@@,,,,:@@@@@@@@@.,,,,,,,.#@@@@@@+,:::::" >> /etc/issue.net
+echo -e ",:::::::,,,,,,,,,,,,,,#@@@@@@@:,,,+@@@@@@@@@.,,,,,,.@@@@@@@;,::::::" >> /etc/issue.net
+echo -e ",::::::,,,,,,,,,,,,,,,+@@@@@@@',,,+@@@@@@@@#,,,,,,:@@@@@@@,:,::::::" >> /etc/issue.net
+echo -e ",:::::,,,,,,,,,,,,,,,,'@@@@@@@#,,,:@@@@@@@@,,,,,.+@@@@@@#.,,,,:::::" >> /etc/issue.net
+echo -e ",:::::,,,,,,,,,,,,,,,,;@@@@@@@@.,,.@@@@@@@@.,,,.#@@@@@@+,,,,,,,::::" >> /etc/issue.net
+echo -e ",::::,,,,,,,,,,,,,,,,,,@@@@@@@@.,,,'@@@@@@.,,,.@@@@@@@:,,,,,,,,::::" >> /etc/issue.net
+echo -e ",::::,,,,,,,,,,,,,,,,,.@@@@@@@@+....#@@@@,,,.:@@@@@@@.,,,,,,,,,::::" >> /etc/issue.net
+echo -e ",:::,,,,,,,,,,,,,,,,,,,#@@@@@@@@.....,''.,,.+@@@@@@#.,,,,,,,,,,,:::" >> /etc/issue.net
+echo -e ",:::,,,,,,,,,,,,,,,,,,,:@@@@@@@@,........,`@@@@@@@'.,,,,,,,,,,,,:::" >> /etc/issue.net
+echo -e ",:::,,,,,,,,,,,,,,,,,,,.@@@@@@@@@.......,,@@@@@@@,,,,,,,,,,,,,,,,::" >> /etc/issue.net
+echo -e ",::,,,,,,,,,,,,,,,,,,,,.+@@@@@@@@,......;@@@@@@@.,,,,,,,,,,,,,,,,::" >> /etc/issue.net
+echo -e ",::,,,,,,,,,,,,,,,,,,,,,.@@@@@@@@@....`#@@@@@@#.,,,,,,,,,,,,,,,,,::" >> /etc/issue.net
+echo -e ",::,,,,,,,,,,,,,,,,,,,...#@@@@@@@@,..`@@@@@@@;.,,,,,,,,,,,,,,,,,,::" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,,,,.....@@@@@@@@@`,@@@@@@@,..,,,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,,,,.....+@@@@@@@@@@@@@@@@`,.,,,,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,,,,.....`@@@@@@@@@@@@@@+`...:.,,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,,.+'.....;@@@@@@@@@@@@:...:@@@;,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,.@@@@`....@@@@@@@@@@@.....@@@@@.,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,+@@@@;.....@@@@@@@@@......@@@@@.,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,.#@@@@;....`@@@@@@@@@@`....@@@@@.,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,:@@@@.....@@@@@@@@@@@'.....@@@.,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,.;@@,...;@@@@@@@@@@@@@........,,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,,,....`#@@@@@@@@@@@@@@#........,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,,,..,.@@@@@@@;@@@@@@@@@,......,,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,,,..,@@@@@@@,..@@@@@@@@@......,,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,,,.'@@@@@@@`...#@@@@@@@@,....,,,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",:,,,,,,,,,,,,,,,,,.#@@@@@@+`....`@@@@@@@@@...,,,,,,,,,,,,,,,,,,,,:" >> /etc/issue.net
+echo -e ",::,,,,,,,,,,,,,,,.@@@@@@@:.......+@@@@@@@@...,,,,,,,,,,,,,,,,,,,::" >> /etc/issue.net
+echo -e ",::,,,,,,,,,,,,,,:@@@@@@@..........@@@@@@@@+.,,,,,,,,,,,,,,,,,,,,::" >> /etc/issue.net
+echo -e ",:::,,,,,,,,,,,.+@@@@@@@`,.........#@@@@@@@@.,,,,,,,,,,,,,,,,,,,,::" >> /etc/issue.net
+echo -e ",:::,,,,,,,,,,.@@@@@@@+.,,.+@@,....,@@@@@@@@,,,,,,,,,,,,,,,,,,,,,::" >> /etc/issue.net
+echo -e ",:::,,,,,,,,,,@@@@@@@:.,,,@@@@@:....@@@@@@@@',,,,,,,,,,,,,,,,,,,:::" >> /etc/issue.net
+echo -e ",:::,,,,,,,,;@@@@@@@.,,,.@@@@@@@....+@@@@@@@#.,,,,,,,,,,,,,,,,,,:::" >> /etc/issue.net
+echo -e ",::::,,,,,.+@@@@@@#.,,,.+@@@@@@@+..,:@@@@@@@@.,,,,,,,,,,,,,,,,,::::" >> /etc/issue.net
+echo -e ",:::::,,,.@@@@@@@+.,,,,.@@@@@@@@@.,,.@@@@@@@@.,,,,,,,,,,,,,,,,,::::" >> /etc/issue.net
+echo -e ",:::::,,:@@@@@@@:,,,,,,,@@@@@@@@@.,,.@@@@@@@@.,,,,,,,,,,,,,,,,:::::" >> /etc/issue.net
+echo -e ",:::::,'@@@@@@@.,,,,,,,'@@@@@@@@@.,,.@@@@@@@@.,,,,,,,,,,,,,,,,:::::" >> /etc/issue.net
+echo -e ",::::,+++#####.,,,,,,,,+@@@@@@@@#,,,.@@@@@@@@.,,,,,,,,,,,,,,,::::::" >> /etc/issue.net
+echo -e ",:::::,,,,,,,,,,,,,,,,,#@@@@@@@@,,,,.@@@@@@@@.,,,,,,,,,,,,,,,::::::" >> /etc/issue.net
+echo -e ",::::::::,,,,,,,,,,,,,,+@@@@@@@#.,,,.@@@@@@@#,,,,,,,,,,,,,,,:::::::" >> /etc/issue.net
+echo -e ",:::::::::,,,,,,,,,,,,,'@@@@@@#.,,,,.@@@@@@@',,,,,,,,,,,,,,::::::::" >> /etc/issue.net
+echo -e ",:::::::::,,,,,,,,,,,,,:@@@@#;.,,,,,,@@@@@@@,,,,,,,,,,,,,,:::::::::" >> /etc/issue.net
+echo -e ",:::::::::::,,,,,,,,,,,.@@@@.,,,,,,,'@@@@@@@.,,,,,,,,,,,,::::::::::" >> /etc/issue.net
+echo -e ",:::::::::::,,,,,,,,,,,,#@@@.,,,,,,.@@@@@@@:,,,,,,,,,,,,:::::::::::" >> /etc/issue.net
+echo -e "::::::::::::::,,,,,,,,,,.@@@',,,,,,:@@@@@@#,,,,,,,,,,,,::::::::::::" >> /etc/issue.net
+echo -e "::::::::::::::,,,,,,,,,,,'@@@:,,,,,@@@@@@@.,,,,,,,,,,,:::::::::::::" >> /etc/issue.net
+echo -e "::::::::::::::::,,,,,,,,,,#@@@+::+@@@@@@@.,,,,,,,,,,:::::::::::::::" >> /etc/issue.net
+echo -e "::::::::::::::::::,,,,,,,,,#@@@@@@@@@@@@.,,,,,,,,,:::::::::::::::::" >> /etc/issue.net
+echo -e ":::::::::::::::::::,,,,,,,,,;@@@@@@@@@',,,,,,,,,:::::::::::::::::::" >> /etc/issue.net
+echo -e "::::::::::::::::::::::,,,,,,,,:+#@#+;,,,,,,,,,:::::::::::::::::::::" >> /etc/issue.net
+echo -e ":::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,:::::::::::::::::::::::" >> /etc/issue.net
+echo -e ":::::::::::::::::::::::::::::::,,,,,,,:::::::::::::::::::::::::::::" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "   _______             _ _                   _      _      _____ " >> /etc/issue.net
+echo -e "  / ____| |           | (_)                 | |    | |    / ____|" >> /etc/issue.net
+echo -e " | (___ | |_ _   _  __| |_  ___  ___  ___   | |    | |   | |     " >> /etc/issue.net
+echo -e "  \___ \| __| | | |/ _` | |/ _ \/ __|/ _ \  | |    | |   | |     " >> /etc/issue.net
+echo -e "  ____) | |_| |_| | (_| | | (_) \__ \ (_) | | |____| |___| |____ " >> /etc/issue.net
+echo -e " |_____/ \__|\__,_|\__,_|_|\___/|___/\___/  |______|______\_____|" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "   _____ _             _ _                   _      _      _____ " >> /etc/issue.net
+echo -e "  / ____| |           | (_)                 | |    | |    / ____|" >> /etc/issue.net
+echo -e " | (___ | |_ _   _  __| |_  ___  ___  ___   | |    | |   | |     " >> /etc/issue.net
+echo -e "  \___ \| __| | | |/ _` | |/ _ \/ __|/ _ \  | |    | |   | |     " >> /etc/issue.net
+echo -e "  ____) | |_| |_| | (_| | | (_) \__ \ (_) | | |____| |___| |____ " >> /etc/issue.net
+echo -e " |_____/ \__|\__,_|\__,_|_|\___/|___/\___/  |______|______\_____|" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e " ______     ______   __     __  __     ______     __     ______   __  __    " >> /etc/issue.net
+echo -e "/\  __ \   /\__  _\ /\ \   /\ \_\ \   /\  __ \   /\ \   /\__  _\ /\ \_\ \   " >> /etc/issue.net
+echo -e "\ \ \/\ \  \/_/\ \/ \ \ \  \ \  __ \  \ \ \/\ \  \ \ \  \/_/\ \/ \ \  __ \  " >> /etc/issue.net
+echo -e " \ \_____\    \ \_\  \ \_\  \ \_\ \_\  \ \_____\  \ \_\    \ \_\  \ \_\ \_\ " >> /etc/issue.net
+echo -e "  \/_____/     \/_/   \/_/   \/_/\/_/   \/_____/   \/_/     \/_/   \/_/\/_/ " >> /etc/issue.net
+echo -e "                                                                            " >> /etc/issue.net
+echo -e " ______     ______   __     __  __     ______     __     ______   __  __    " >> /etc/issue.net
+echo -e "/\  __ \   /\__  _\ /\ \   /\ \_\ \   /\  __ \   /\ \   /\__  _\ /\ \_\ \   " >> /etc/issue.net
+echo -e "\ \ \/\ \  \/_/\ \/ \ \ \  \ \  __ \  \ \ \/\ \  \ \ \  \/_/\ \/ \ \  __ \  " >> /etc/issue.net
+echo -e " \ \_____\    \ \_\  \ \_\  \ \_\ \_\  \ \_____\  \ \_\    \ \_\  \ \_\ \_\ " >> /etc/issue.net
+echo -e "  \/_____/     \/_/   \/_/   \/_/\/_/   \/_____/   \/_/     \/_/   \/_/\/_/ " >> /etc/issue.net
+echo -e "                                                                            " >> /etc/issue.net
+echo -e "" >> /etc/issue.net
+echo -e "Вы были предупреждены." >> /etc/issue.net
+echo -e "你被警告了。" >> /etc/issue.net
+echo -e "당신은 경고를 받았습니다" >> /etc/issue.net.
+echo -e "" >> /etc/issue.net
+echo -e "###############################################" >> /etc/issue.net
+echo -e "#You have been warned. (^^^^Go to the top^^^^)# " >> /etc/issue.net
+echo -e "#                                             #" >> /etc/issue.net
+echo -e "#You have been warned. (^^^^Go to the top^^^^)#" >> /etc/issue.net
+echo -e "#You have been warned. (^^^^Go to the top^^^^)#" >> /etc/issue.net
+echo -e "#                                             #" >> /etc/issue.net
+echo -e "#You have been warned. (^^^^Go to the top^^^^)#" >> /etc/issue.net
+echo -e "###############################################" >> /etc/issue.net
