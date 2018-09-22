60e3d942dac036bf462db79fcfd0b7f4b45ee7ad
Charles Porth <charlieporth1@gmail.com>
2018-09-22 17:52:26 -0500

    Delete email-virus-report.sh

--- email-virus-report.sh
+++
@@ -1,8 +0,0 @@
-#bash
-#uuencode /mnt/HDD/virus.txt | sendemail -f ServerVirusReport@gmail.com -t charlieporth1@gmail.com -u subject -m "Virus Report from server" -s smtp.gmail.com:587 -o tls=yes -xu chtiporth@gmail.com -xp sciencerocks 
-# echo Virus Report | mutt -s Virus Report from server -a /mnt/HDD/virus.txt -- charlieporth1@gmail.com
-. /usr/sbin/emailcred.sh 
-sendemail -f $USER@otih-oith.us.to -t charlieporth1@gmail.com -m "Virus Report from server $HOSTNAME" -a  /mnt/HDD/virus.txt -s smtp.gmail.com:587 -o tls=yes -xu chtiporth@gmail.com -xp $(echo $passwd)
-#sendemail -f $USER@otih-oith.us.to -t 19523341587@txt.att.net -m "Virus Report from server" -a  /mnt/HDD/virus.txt -s smtp.gmail.com:587 -o tls=yes -xu chtiporth@gmail.com -xp sciencerocks 
-sendemail -f $USER@otih-oith.us.to -t 9523341587@mms.att.net  -m "Virus Report from server" -a  /mnt/HDD/virus.txt -s smtp.gmail.com:587 -o tls=yes -xu chtiporth@gmail.com -xp (echo $passwd)
-echo done
