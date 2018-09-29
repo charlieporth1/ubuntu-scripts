5e0f22a3ebec1ecbd63000a4dc344ceecef7fe70
root <root@otih-oith.us.to>
2018-09-29 00:05:56 -0500

    Sat Sep 29 00:05:55 CDT 2018

--- phoneone.sh
+++ phoneone.sh
@@ -18,9 +18,10 @@ export alert="alert"
 export owner="owner"
 export Login="Login"
 export IP="IP"
+export tracked="tracked"
 echo -e "$yellow$Login $red$alert$nc sent to the $yellow$owner$nc of this server"
 export llll=$(last  -1  --time-format notime | grep -e  'ubuntu\|logged in\|pts/*\|tty*' |  sed -n '1p' |  grep -e  'ubuntu\|logged in\|pts/*\|tty*' |  awk '{print $3}'  )
-echo -e "Your IP has been tracked; your $yellow$IP$nc address is $red$llll$nc"
+echo -e "Your IP has been $yellow$tracked$nc; your $yellow$IP$nc address is $red$llll$nc"
 echo -e "If you log-off nothing will happen to you; if not your fucked"
 . /usr/bin/cred.sh
 sendemail -f $USER@otih-oith.us.to -t $phonee  -m "Someone has logged into your one of your servers under the User: $USER; and the Server: $HOSTNAME; at the time of $(date); the external IP address of the login was from $llll; the GeoIP $(geoiplookup $llll | grep -i city | cut -d ':' -f 2 | cut -d ',' -f -7 ); the ISP of the login  $(geoiplookup $llll | grep -i ASNum | cut -d ':' -f 2  )"   -s smtp.gmail.com:587 -o tls=yes -xu $usr -xp  $passwd | grep -i --color=auto successfully!
