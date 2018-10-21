e52330d58ff653b690a1d7604541fb6860719f17
root <root@otih-oith.us.to>
2018-10-21 00:17:52 -0500

    Sun Oct 21 00:17:51 CDT 2018

--- phonetext.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -O -s 'https://api.twilio.com/2010-04-01/Accounts/AC473961505e8705270a1cfe08f453a090/Messages.json' -X POST \
---data-urlencode 'To=+19523341587' \
---data-urlencode 'From=+17634028391' \
---data-urlencode 'Body=Someone Has Logged into one of your servers' \
--u AC473961505e8705270a1cfe08f453a090:b150148870c3781b0de90f61f46f9db4  > /dev/null
-exit 0
