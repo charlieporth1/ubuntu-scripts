83883c6241b883590f27c517792c06ec47ef70e4
Charles Porth <charlieporth1@gmail.com>
2019-06-01 00:01:26 -0500

    Sat Jun  1 00:01:26 CDT 2019

--- toGithub.sh
+++ toGithub.sh
@@ -9,22 +9,6 @@ function gitIgnore() {
 	git ignore ".git/*"
 	git ignore "*.git/*"
 }
-function removeSubModules() {
-	submodules=($(git config --file .gitmodules --get-regexp path | awk '{ print $2 }'))
-
-	# Loop over submodules and convert to regular files
-	for submodule in "${submodules[@]}"
-	do  
-	   echo "Removing $submodule"
-	   git rm --cached $submodule # Delete references to submodule HEAD
-	   rm -rf $submodule/.git* # Remove submodule .git references to prevent confusion from main repo
-	   git add $submodule # Add the left over files from the submodule to the main repo
- 	   git commit -m "Converting submodule $submodule to regular files" # Commit the new regular files!
-	done
-
-	# Finally remove the submodule mapping
-	git rm.gitmodules
-}
 function toGit() {
 	dir=$1
 	ifClus=false
@@ -39,13 +23,13 @@ function toGit() {
 	fi
 	gitIgnore
 	sleep 5s
+	git submodule deinit --all
 	git add .
-	removeSubModules
 	if [[ `git status --porcelain` ]]; then
 	  # Changes
 		echo changes
 		gitIgnore
-		removeSubModules
+		git submodule deinit --all
 		sleep 5s
 		git add  . #| parallel $cluster
 		removeSubModules
